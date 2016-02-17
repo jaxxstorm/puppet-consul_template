14e27dd1c7c9af521d442de76cf412105db5eb7a
Lee Briggs <lee@leebriggs.co.uk>
2016-02-17 21:45:14 +0000

    Yet more clean up

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,22 +3,9 @@
 # This class is called from consul_template for service config.
 #
 class consul_template::config (
-  $consul_host,
-  $consul_port,
-  $consul_token,
-  $consul_retry,
   $config_hash,
   $purge = true,
 ) {
-
-  # Set wait param if specified
-  if $::consul_template::consul_wait {
-    concat::fragment { 'consul_wait':
-      target  => 'consul-template/config.json',
-      content => inline_template("wait = \"${::consul_template::consul_wait}\"\n\n"),
-      order   => '02',
-    }
-  }
 
   file { [$consul_template::config_dir, "${consul_template::config_dir}/config"]:
     ensure  => 'directory',
